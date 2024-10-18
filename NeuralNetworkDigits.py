c4ea63cdb85d5f8327042984f69f0e5987841e84
Guyonetwo <hudsonaustin25@gmail.com>
2024-10-18 13:10:20 -0500

    Adding tkinter window
    Added start of tkinter window for custom digits to be testing by the network.

--- NeuralNetworkDigits.py
+++ NeuralNetworkDigits.py
@@ -3,6 +3,9 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import tkinter as tk
+from tkinter import ALL, EventType
+from tkinter import colorchooser
 
 class layer:
     def __init__(self, numInputs, numNeurons):
@@ -98,22 +101,22 @@ numCorrect = 0
 total = 0
 sets = 1
 
-#np.random.shuffle(Xtrain)
-
-#plt.ion()
-
 for shuffle in range(sets):
     for i in range(iterations):
         Xtest = np.zeros((1, 784))
         Xtest[0] = Xtrain[i]
         forwardProp(layer1, layer2, activation1, activation2, Xtest)
         dW1, db1, dW2, db2 = backProp(layer1, layer2, activation1, activation2, Xtest, ytrain[i])
-        updateParameters(layer1, layer2, dW1, db1, dW2, db2, 0.008)
+        updateParameters(layer1, layer2, dW1, db1, dW2, db2, 0.03)
         #print(get_predictions(activation2.outputs) ,ytrain[i])
         if(get_predictions(activation2.outputs) == ytrain[i]):
             numCorrect += 1
         total += 1
-    #np.random.shuffle(Xtrain.T)
+
+# df = pd.DataFrame(layer1.weights)
+# df.to_csv("weights.csv", header=False, index=False)
+# df = pd.read_csv("weights.csv")
+# print(layer1.weights.shape, df)
 
 #testNumber = input("What prediction?")
 # while(not(testNumber == -1)):
@@ -121,4 +124,28 @@ for shuffle in range(sets):
 #     time.sleep(1)
 #     testNumber = input("What prediction?")
 
-#print(numCorrect/total)
+print(numCorrect/total)
+
+def paint(e):
+    x1, y1 = e.x-1, e.y-1
+    x2, y2 = e.x, e.y
+    canvas.create_line(x1,y1,x2,y2,fill='white', width=1)
+
+def do_zoom(event):
+    x = canvas.canvasx(event.x)
+    y = canvas.canvasy(event.y)
+    factor = 1.001 ** event.delta
+    canvas.scale(ALL, x, y, factor, factor)
+
+root = tk.Tk()
+root.title("Digit Recognition Demo")
+root.geometry("800x800")
+
+brushColor = "black"
+
+canvas = tk.Canvas(root, width=28, height=28, bg="black")
+canvas.pack()
+canvas.bind('<B1-Motion>', paint)
+canvas.bind("<MouseWheel>", do_zoom) # WINDOWS ONLY
+
+root.mainloop()
