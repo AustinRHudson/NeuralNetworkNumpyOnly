607e67d0e9a99079a22265a0aefd9a87fecf01fb
Austin R Hudson <hudsonaustin25@gmail.com>
2024-10-18 13:58:53 -0500

    Update NeuralNetworkDigits.py

--- NeuralNetworkDigits.py
+++ NeuralNetworkDigits.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import tkinter as tk
 from tkinter import ALL, EventType
-from tkinter import colorchooser
+from PIL import Image, ImageGrab
 
 class layer:
     def __init__(self, numInputs, numNeurons):
@@ -107,7 +107,7 @@ for shuffle in range(sets):
         Xtest[0] = Xtrain[i]
         forwardProp(layer1, layer2, activation1, activation2, Xtest)
         dW1, db1, dW2, db2 = backProp(layer1, layer2, activation1, activation2, Xtest, ytrain[i])
-        updateParameters(layer1, layer2, dW1, db1, dW2, db2, 0.03)
+        updateParameters(layer1, layer2, dW1, db1, dW2, db2, 0.008)
         #print(get_predictions(activation2.outputs) ,ytrain[i])
         if(get_predictions(activation2.outputs) == ytrain[i]):
             numCorrect += 1
@@ -129,7 +129,7 @@ print(numCorrect/total)
 def paint(e):
     x1, y1 = e.x-1, e.y-1
     x2, y2 = e.x, e.y
-    canvas.create_line(x1,y1,x2,y2,fill='white', width=1)
+    canvas.create_line(x1,y1,x2,y2,fill='white', width=3, capstyle="round", arrow=tk.BOTH)
 
 def do_zoom(event):
     x = canvas.canvasx(event.x)
@@ -143,9 +143,21 @@ root.geometry("800x800")
 
 brushColor = "black"
 
-canvas = tk.Canvas(root, width=28, height=28, bg="black")
-canvas.pack()
+canvas = tk.Canvas(root, width=800, height=800, bg="black")
+canvas.create_bitmap((28,28))
 canvas.bind('<B1-Motion>', paint)
 canvas.bind("<MouseWheel>", do_zoom) # WINDOWS ONLY
+canvas.pack()
+
+def getter(widget):
+    x=root.winfo_rootx()+widget.winfo_x()
+    y=root.winfo_rooty()+widget.winfo_y()
+    x1=x+widget.winfo_width()
+    y1=y+widget.winfo_height()
+    ImageGrab.grab().crop((x,y,x1,y1)).save("testImage.png")
 
 root.mainloop()
+
+time.sleep(3)
+
+getter(root)
